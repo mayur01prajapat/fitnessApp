b69002a4a8ce1120851e4c2f422193c0564b2cb5
Mayur Prajapat <mayur01prajapat@gmail.com>
2020-03-26 21:31:40 +0530

    Initial commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -55,7 +55,7 @@
 
 <script>
 export default {
-    name: 'HelloWorld',
+    name: "Login",
     props: {
         msg: String
     }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Login from './components/Login.vue';
+import Profile from './components/Profile.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/login', component: Login },
+  { path: '/profile', component: Profile },
+];
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
