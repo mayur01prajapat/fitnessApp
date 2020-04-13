6ee2c29faa5bdb3dcdd7b6315a29aa95cfdba6bb
prateek.trivedi.25@gmail.com <prateek.trivedi.25@gmail.com>
2020-04-13 18:09:33 +0530

    fixed url

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,14 +7,14 @@ const posts = require("../models/Posts");
 const router = express.Router();
 
 router
-  .get("/posts/all", (req, res) => res.send(posts.getPosts()))
-  .get("/posts/:id", (req, res) =>
+  .get("/all", (req, res) => res.send(posts.getPosts()))
+  .get("/:id", (req, res) =>
     res.send(posts.getExerciseDetails(req.params.id))
   )
-  .get("/posts/save", (req, res) =>
+  .get("/save", (req, res) =>
     res.send(posts.addPosts(req.body.excersise))
   )
-  .get("/posts/delete", (req, res) =>
+  .get("/delete", (req, res) =>
     res.send(posts.deletePosts(req.params.id))
   );
 
